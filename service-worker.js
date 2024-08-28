3f25db800d927e4caad04c714118682ae8ba70f7
fafunblocked <mirzajunaid3111@gmail.com>
2024-08-28 15:33:36 +0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,28 @@
-const cacheName = 'faf-games-cache-v1';
-const filesToCache = [
+const CACHE_NAME = 'my-site-cache-v1';
+const urlsToCache = [
   '/',
-  '/index.html',
-  '/styles.css',
-  '/script.js',
-  '/path/to/icon-192x192.png',
-  '/path/to/icon-512x512.png'
+  '/styles/main.css',
+  '/script/main.js',
+  '/offline.html'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(cacheName).then(cache => {
-      return cache.addAll(filesToCache);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
   );
 });
