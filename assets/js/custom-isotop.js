9e7aeb7b117edc4a455287924eeca5626852090c
fafunblocked <mirzajunaid3111@gmail.com>
2024-08-28 17:56:39 +0500

    Update custom-isotop.js

--- assets/js/custom-isotop.js
+++ assets/js/custom-isotop.js
@@ -1,3 +1,14 @@
+// Google Analytics code
+		(function() {
+			$.getScript('https://www.googletagmanager.com/gtag/js?id=G-6BPGNZNTLZ', function() {
+				window.dataLayer = window.dataLayer || [];
+				function gtag(){ dataLayer.push(arguments); }
+				gtag('js', new Date());
+				gtag('config', 'G-6BPGNZNTLZ');
+			});
+		})();
+
+
 $(window).on('load', function () {
 
     // Dynamically add the manifest link
