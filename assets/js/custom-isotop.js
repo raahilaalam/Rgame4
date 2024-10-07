129916c35d48e56469004aafa40a48f9f1e803a5
fafunblocked <mirzajunaid3111@gmail.com>
2024-10-07 20:15:36 +0500

    Update custom-isotop.js

--- assets/js/custom-isotop.js
+++ assets/js/custom-isotop.js
@@ -65,7 +65,7 @@ function setupPwaInstallation() {
         const popupHTML = `
             <div id="pwa-popup" style="display: none; position: fixed; top: 0; left: 0; width: 100%; height: 100%; background: rgba(255,255,255,0.8); color: #333; text-align: center; z-index: 1000; display: flex; align-items: center; justify-content: center;">
                 <div style="padding: 25px; background: #f5f5f5; border-radius: 20px; width: 90%; max-width: 450px; box-shadow: 0 4px 15px rgba(0, 0, 0, 0.2); text-align: center;">
-                    <h2 style="font-size: 22px; margin-bottom: 15px; color: #2c3e50;">Install the app</h2>
+                    <h2 style="font-size: 22px; margin-bottom: 15px; color: #2c3e50;">Add Our App for a Faster, Seamless Experience!</h2>
                     <button id="install-button" style="padding: 12px 28px; font-size: 18px; cursor: pointer; background: #7f2525; color: white; border: none; border-radius: 30px;">Add to Home Screen</button>
                     <button id="close-popup" style="padding: 12px 28px; font-size: 18px; cursor: pointer; background-color: transparent; color: #888; border: none; border-radius: 30px;">Not Now</button>
                 </div>
